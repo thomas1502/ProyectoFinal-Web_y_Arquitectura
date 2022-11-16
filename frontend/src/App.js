ad149e0f964acf32d00037eb93e850d203cf1e00
GersonPMAM <gerson_@umes.edu.gt>
2022-11-16 09:49:26 -0600

    Rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import data from './data';
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div >
-      <header >
-        <a href="/">Store Online S.A.</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {
-            data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>              
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p> {product.name} </p>
-                </a>   
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>              
-            </div>))
-          }
-        </div>        
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Store Online S.A.</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen/>}/>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
