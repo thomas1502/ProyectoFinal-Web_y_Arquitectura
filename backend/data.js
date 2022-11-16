1417e466c16872a025264abf7db20ce4db5f8b17
thomas1502 <thomas1502@mesoamericana.edu.gt>
2022-11-16 10:02:13 -0600

    Creacion de Node

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg', // 679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt',
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 25,
+            countInStock: 15,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'High quality product',
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInStock: 5,
+            brand:'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product',
+        },
+    ],            
+};
+
+export default data;
