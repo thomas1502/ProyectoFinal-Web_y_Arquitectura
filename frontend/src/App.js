06c9fd1c0f2c30b03d223e09d02747503f154191
thomas1502 <thomas1502@mesoamericana.edu.gt>
2022-11-15 23:15:55 -0600

    Avances de tienda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -7,7 +8,28 @@ function App() {
       <header className="App-header">
         <a href="/">Store Online S.A.</a>
       </header>
-      <main>Lista de productos</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {
+            data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>              
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p> {product.name} </p>
+                </a>   
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>              
+            </div>))
+          }
+        </div>        
+      </main>
     </div>
   );
 }
