27d7fca8cc7a6402c62083267a3ef2d074b3b076
GersonPMAM <gerson_@umes.edu.gt>
2022-11-16 22:14:00 -0600

    Vista Producto

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,19 @@ const app = express();
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
+app.get('/api/products/slug/:slug', (req, res) => {
+    const product = data.products.find((x)=> x.slug === req.params.slug);
+    if(product){
+        res.send(product);
+    }else{
+        res.status(404).send({message: 'Product Not Fund'});
+    }
+    res.send(data.products);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serve running at http://localhost:${port}`)
 });
+
 
