97f162d9df98caa515985b4c5ad8f972a649ce0b
GersonPMAM <gerson_@umes.edu.gt>
2022-11-17 17:22:03 -0600

    Api

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,15 +8,14 @@ productRouter.get('/', async(req, res) => {
     res.send(products);
 });
 
-productRouter.get("/slug/:slug", async (req, res) => {
-    const product = await Product.findOne({ slug: req.params.slug });
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: "Product Not Fund" });
-    }
-    res.send(data.products);
-  });
+productRouter.get('/slug/:slug', async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
   
   productRouter.get("/:id", async (req, res) => {
     const product = await Product.findById(req.params.id);
