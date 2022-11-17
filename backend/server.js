61c3e93fdb5c461c1cf073795ab89f029767ee1e
GersonPMAM <gerson_@umes.edu.gt>
2022-11-17 16:25:20 -0600

    Conexion a mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,44 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Conectado a db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
-app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find((x)=> x.slug === req.params.slug);
-    if(product){
-        res.send(product);
-    }else{
-        res.status(404).send({message: 'Product Not Fund'});
-    }
-    res.send(data.products);
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Fund" });
+  }
+  res.send(data.products);
 });
 
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x)=> x._id === req.params.id);
-    if(product){
-        res.send(product);
-    }else{
-        res.status(404).send({message: 'Product Not Fund'});
-    }
-    res.send(data.products);
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Fund" });
+  }
+  res.send(data.products);
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Serve running at http://localhost:${port}`)
+  console.log(`Serve running at http://localhost:${port}`);
 });
-
-
