a13d0f16c7c5b0dcb831302f0212e4414805997f
GersonPMAM <gerson_@umes.edu.gt>
2022-11-17 10:02:09 -0600

    Conteo de productos

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
     products: [
         {
+            _id: '1',
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt',
             category: 'Shirts',
@@ -13,6 +14,7 @@ const data = {
             description: 'High quality shirt',
         },
         {
+            _id: '2',
             name: 'Nike Slim Pant',
             slug: 'nike-slim-pant',
             category: 'Pants',
@@ -25,6 +27,7 @@ const data = {
             description: 'High quality product',
         },
         {
+            _id: '3',
             name: 'Adidas Fit Pant',
             slug: 'adidas-fit-pant',
             category: 'Pants',
