4ca2642a90b08be8d0a589489c0450270665987d
thomas1502 <thomas1502@mesoamericana.edu.gt>
2022-11-17 11:19:38 -0600

    Pantalla de registro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import {LinkContainer} from 'react-router-bootstrap';
 import { useContext } from "react";
 import { Store } from "./screens/Store";
 import CartScreen from './screens/CartScreen';
+import SigninScreen from "./screens/SigninScreen";
 
 
 function App() {
@@ -45,6 +46,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen/>}/>
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
