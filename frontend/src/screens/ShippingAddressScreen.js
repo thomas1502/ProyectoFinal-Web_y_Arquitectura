49b067d234c04fc2fef0adac5383470f00f2fd09
GersonPMAM <gerson_@umes.edu.gt>
2022-11-18 11:11:09 -0600

    L2

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -1,10 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet-async';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
-import { Store } from './Store';
-import CheckoutSteps from '../components/CheckoutSteps';
+import React, { useContext, useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Store } from "./Store";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 export default function ShippingAddressScreen() {
   const navigate = useNavigate();
@@ -16,19 +16,17 @@ export default function ShippingAddressScreen() {
   const [fullName, setFullName] = useState(shippingAddress.fullName || '');
   const [address, setAddress] = useState(shippingAddress.address || '');
   const [city, setCity] = useState(shippingAddress.city || '');
-  const [postalCode, setPostalCode] = useState(
-    shippingAddress.postalCode || ''
-  );
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || '');
   useEffect(() => {
-    if (!userInfo) {
-      navigate('/signin?redirect=/shipping');
+    if(!userInfo){
+        navigate('/signin?redirect=/shipping');
     }
-  }, [userInfo, navigate]);
+  }, [userInfo, navigate])
   const [country, setCountry] = useState(shippingAddress.country || '');
   const submitHandler = (e) => {
     e.preventDefault();
     ctxDispatch({
-      type: 'SAVE_SHIPPING_ADDRESS',
+      type: "SAVE_SHIPPING_ADDRESS",
       payload: {
         fullName,
         address,
@@ -38,7 +36,7 @@ export default function ShippingAddressScreen() {
       },
     });
     localStorage.setItem(
-      'shippingAddress',
+      "shippingAddress",
       JSON.stringify({
         fullName,
         address,
@@ -47,14 +45,14 @@ export default function ShippingAddressScreen() {
         country,
       })
     );
-    navigate('/payment');
+    navigate("/payment");
   };
+
   return (
     <div>
       <Helmet>
         <title>Shipping Address</title>
       </Helmet>
-
       <CheckoutSteps step1 step2></CheckoutSteps>
       <div className="container small-container">
         <h1 className="my-3">Shipping Address</h1>
