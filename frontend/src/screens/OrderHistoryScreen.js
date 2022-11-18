576185094e317598e83b2d90b095cc2a5b03fc4f
GersonPMAM <gerson_@umes.edu.gt>
2022-11-18 15:46:10 -0600

    Actualizar usuario

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-import { Store } from '../Store';
+import { Store } from './Store';
 import { getError } from '../utils';
 import Button from 'react-bootstrap/esm/Button';
 
