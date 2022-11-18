325d68919899b8445d2829f238e6c8cf098d2baa
GersonPMAM <gerson_@umes.edu.gt>
2022-11-18 11:49:37 -0600

    Nueva version de node

--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import CheckoutSteps from '../components/CheckoutSteps';
-import { Store } from '../Store';
+import { Store } from './Store';
 
 export default function PaymentMethodScreen() {
   const navigate = useNavigate();
