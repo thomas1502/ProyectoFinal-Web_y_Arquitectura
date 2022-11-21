9d572053b5781c23bef2abf4ae808f38acc51ef9
GersonPMAM <gerson_@umes.edu.gt>
2022-11-20 23:22:10 -0600

    Funciona porfavor

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
- 
+  
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
